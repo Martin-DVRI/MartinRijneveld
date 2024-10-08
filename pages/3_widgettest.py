a7a45cde857bcff7d598806b5cb722bf09a60517
Martin-DVRI <m.rijneveld@duravermeer.nl>
2024-10-08 17:52:36 +0200

    Update 3_widgettest.py

--- pages/3_widgettest.py
+++ pages/3_widgettest.py
@@ -11,7 +11,7 @@ st.title('Test-pagina voor widgets')
 'Dit is een pagina om de verschillende widgets te testen'
 
 var1=st.slider('eerste getal:',min_value=0.0, max_value=100.0, step=1.0)
-var2=st.number_input('tweede getal?',min_value=0.0, max_value=10, step=0.5)
+var2=st.number_input('tweede getal?',min_value=0.0, max_value=10.0, step=0.5)
 operator=st.radio('kies een actie',['plus','min','keer','gedeeld door'])
 if operator=='plus':
     answer = var1 + var2
